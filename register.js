9e47be40879bf2e872f130177860f298103f5ca5
kessyrezende <71460863+kessyrezende@users.noreply.github.com>
2020-11-30 23:54:59 -0300

    criacao do register

---
+++ register.js
@@ -0,0 +1,5 @@
+if("serviceMorker" in navigator){
+   navigator.serviceMorker.register("sw.js")
+   .then(function(){console.log("serviceMorker esta registrado")})
+   .cath(function(){console.log("Erro ao registrar serviceMorker")})
+}
